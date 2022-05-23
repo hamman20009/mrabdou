c1e60267605077e334e00114ee84947594176192
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-05-23 20:31:56 +0200

    Update products.js

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/1.png',
+		image: 'img/1.jpg',
 		//image2: //'img/products/2.png',
 		//image3: //'img/products/3.png',
 		//image4: //'img/products/4.png',
