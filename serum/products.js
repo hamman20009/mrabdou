3fb5ddc7112414bbdb78bd8efbd12f4972ccda53
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-07-18 20:03:03 +0200

    Update products.js

--- serum/products.js
+++ serum/products.js
@@ -7,10 +7,10 @@ const products = [
 		image3: 'img/products/1.jpg',
 		image4: 'img/products/1.jpg',
 		image5: 'img/products/1.jpg',
-		title: '  سيروم ضدالصلع و تساقط الشعر  ',
+		title: ' علاج النهائي للدولي و الارجل الثقيلة   ',
 		price1: 3900,
 		price: 3500,
-		description: ' سيروم فيتوغان ضد تساقط الشعر صالح لحالات الصلع الوراثي و لتساقط الشعر يعمل على اعادى تنشيط بويصلات الشعر لاعادة انباته مرة اخرى '   ,
+		description: '  '   ,
 		phone: 0696711953,
 	},
 
