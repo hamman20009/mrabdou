ea079571907d58cbcd0540949ebefb3b70b24d9a
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-07-29 00:23:31 +0200

    Update products.js

--- serum/products.js
+++ serum/products.js
@@ -2,16 +2,16 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/1.jpg',
-		image2: 'img/products/1.jpg',
-		image3: 'img/products/1.jpg',
-		image4: 'img/products/1.jpg',
-		image5: 'img/products/1.jpg',
-		title: ' علاج النهائي للدولي و الارجل الثقيلة   ',
+		image: 'img/products/1.png',
+		image2: 'img/products/1.png',
+		image3: 'img/products/1.png',
+		image4: 'img/products/1.png',
+		image5: 'img/products/1.png',
+		title: ' علاج النهائي لمرض السكري و تنشيط البنكرياس   ',
 		price1: 3900,
 		price: 3500,
 		description: '  '   ,
-		phone: 0696711953,
+		phone: 0554139163,
 	},
 
 	
