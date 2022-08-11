cbcef6e5cac24b340e73dc9ae83ca6ab8bfcef5e
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-08-11 21:00:14 +0200

    Update products.js

--- products.js
+++ products.js
@@ -2,11 +2,11 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/1.png',
+		image: 'img/1.jpg',
 		
-		title: 'العلاج النهائي لنوبة الهلع     '  ,
+		title: 'علاج تساقط الشعر الاصلي و الناجح في الجزائر لمدة ثلاث اشهر فقط      '  ,
 		price1: 4000,
-		price: 2900,
+		price: 3500,
 		description: '    '  ,
 		phone:0554139163,
 	},
