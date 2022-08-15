673b1d0c9b4a2e3ec8ccf59a8f800db3f3153383
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-08-15 14:23:18 +0200

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
 		logo:'img/logo.png',
 		image: 'img/1.jpg',
 		
-		title: 'علاج تساقط الشعر الاصلي و الناجح في الجزائر لمدة ثلاث اشهر فقط      '  ,
+		title: 'العسل الملكي باك للرجال المتزوجين       '  ,
 		price1: 4000,
 		price: 3500,
 		description: '    '  ,
